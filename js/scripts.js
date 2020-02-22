8054f71538d2109811fd8ed02d59abfe890c22b8
Jamie Gobeille <djent34@gmail.com>
2020-02-22 16:30:40 +0900

    added feature to append user info to page from API

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,8 @@ https://randomuser.me/
 
 //vars
 const randomUsersUrl =
-  "https://randomuser.me/api/?results=12&nat=us&inc=image,name,email,location";
+  "https://randomuser.me/api/?results=12&nat=us&inc=picture,name,email,location";
+const gallery = document.querySelector(".gallery");
 
 //Handle all Fetch requests
 const getJSON = async url => {
@@ -39,9 +40,31 @@ const getJSON = async url => {
   }
 };
 
+//get the users
 const getRandomUsers = async url => {
   const users = await getJSON(url);
-  console.log(users);
+  users.results.map(user => {
+    generateHTML(user);
+    console.log(user);
+  });
+};
+
+// Generate HTML and append it to the page
+
+const generateHTML = person => {
+  const card = document.createElement("div");
+  card.className = "card";
+  gallery.appendChild(card);
+  card.innerHTML = `
+      <div class="card-img-container">
+          <img class="card-img" src="${person.picture.medium}" alt="profile picture">
+      </div>
+      <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+          <p class="card-text">${person.email}</p>
+          <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+      </div>
+        `;
 };
 
 getRandomUsers(randomUsersUrl);
